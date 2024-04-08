0df5049d284051ab69e385461b0b47df061471b9
Christian <generalpride@gmail.com>
2024-04-08 20:22:54 +0800

    init

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import './assets/styles/index.css';
+import './assets/styles/bootstrap.custom.css';
+import App from './App';
+import HomePage from './pages/Home';
+import ProductDetailsPage from './pages/ProductDetails';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomePage />} />
+      <Route path='/product/:id' element={<ProductDetailsPage />} />
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/products.js
@@ -0,0 +1,17 @@
+const products = [
+	{
+		_id: '1',
+		name: 'Airpods',
+		image: '/images/airpods.jpg',
+		description: 'bluetooth',
+		brand: 'Apple',
+		category: 'Electronics',
+		price: 89.90,
+		countInStock: 10,
+		rating: 4.5,
+		numReviews: 10,
+	}
+];
+
+
+export default products;
