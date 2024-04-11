68f359b4e0696ed650dacd8a5790bd9b36b0d555
Christian <generalpride@gmail.com>
2024-04-11 16:41:42 +0800

    added zustand to get and store data

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { productStore } from '../store/products';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const HomePage = () => {
-  const [products, setProducts] = useState([]);
+  const { data, isLoading, fetchProducts } = productStore();
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data: { data = [] } } = await axios.get(`${BACKEND_URL}/api/products`);
-
-      setProducts(data);
-    }
-
     fetchProducts();
   }, []);
 
@@ -21,11 +14,11 @@ const HomePage = () => {
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
+        {!isLoading ? data.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
-        ))}
+        )) : <p>Loading...</p>}
       </Row>
     </>
   );
